1594b0268a07708899288dc6b1865b3065cdf95f
AronRP <aron16d@gmail.com>
2023-01-22 15:15:19 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalModule } from './principal/principal.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrincipalModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
